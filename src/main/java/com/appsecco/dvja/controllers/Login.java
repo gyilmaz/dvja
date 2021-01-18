c9da4b7183ccac8633d92badb5874f8e7b3ad566
gyilmaz <guvenyilmaz@gmail.com>
2021-01-18 13:35:58 -0600

    Update Login.java
    coment login class

--- src/main/java/com/appsecco/dvja/controllers/Login.java
+++ src/main/java/com/appsecco/dvja/controllers/Login.java
@@ -6,39 +6,39 @@ import org.apache.commons.lang.StringUtils;
 
 public class Login extends BaseController {
 
-    private UserAuthenticationService userAuthenticationService;
+//     private UserAuthenticationService userAuthenticationService;
 
-    private String login;
-    private String password;
+//     private String login;
+//     private String password;
 
-    public UserAuthenticationService getUserAuthenticationService() {
-        return userAuthenticationService;
-    }
+//     public UserAuthenticationService getUserAuthenticationService() {
+//         return userAuthenticationService;
+//     }
 
-    public void setUserAuthenticationService(UserAuthenticationService userAuthenticationService) {
-        this.userAuthenticationService = userAuthenticationService;
-    }
+//     public void setUserAuthenticationService(UserAuthenticationService userAuthenticationService) {
+//         this.userAuthenticationService = userAuthenticationService;
+//     }
 
-    public String getLogin() {
-        return login;
-    }
+//     public String getLogin() {
+//         return login;
+//     }
 
-    public void setLogin(String login) {
-        this.login = login;
-    }
+//     public void setLogin(String login) {
+//         this.login = login;
+//     }
 
-    public String getPassword() {
-        return password;
-    }
+//     public String getPassword() {
+//         return password;
+//     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+//     public void setPassword(String password) {
+//         this.password = password;
+//     }
 
-    public String logout() {
-        sessionRemoveUser();
-        return SUCCESS;
-    }
+//     public String logout() {
+//         sessionRemoveUser();
+//         return SUCCESS;
+//     }
 
 //     public String execute() {
 //         User user;
